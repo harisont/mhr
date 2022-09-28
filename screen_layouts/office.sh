93a02bca1bed7702c7b87e60b3804b7f977c56e3
Arianna Masciolini <uzkamascio@gmail.com>
2022-09-28 11:08:50 +0200

    better office layout

--- screen_layouts/office.sh
+++ screen_layouts/office.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1280x720 --pos 1920x360 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output DP-2 --off --output HDMI-3 --off --output DP-2-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2-2 --off --output DP-2-3 --off
+xrandr --output eDP-1 --mode 1600x900 --pos 1920x180 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output DP-2 --off --output HDMI-3 --off --output DP-2-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2-2 --off --output DP-2-3 --off
